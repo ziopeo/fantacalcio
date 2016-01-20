4451be4ebd594932d35235d6e72f1f04c66d2e36
Stefano Foresta <forestastefano@gmail.com>
2016-01-20 18:11:18 +0100

    Inserimento home page per utente collegata all'index senza modulo di login

---
+++ Fantacalcio/WebApplication/home.php
@@ -0,0 +1,26 @@
+<?php
+	echo "<!doctype html public>
+<html dir=\"ltr\" lang=\"IT\">
+	<head>
+		<meta charset=\"UTF-8\">
+        <meta name=\"description\" content=\"UFL - University Fanta League - website application per sfidare i tuoi compagni universitari in una fantalega\">
+        <meta name=\"keywords\" content=\"università facoltà lega fantalega fantacalcio calcio soccer fantamilioni mercato calciatori\">
+        <meta name=\"author\" content=\"Foresta Stefano, Franzese Gennaro, Paglialonga Giuseppe\">
+        <meta name=\"copyright\" content=\"Foresta Stefano, Franzese Gennaro, Paglialonga Giuseppe, UFL\">
+        <meta name=\"robots\" content=\"index,follow\">
+		<title>UFL - University Fanta League</title>
+		<link href=\"css/principale.css\" rel=\"stylesheet\" type=\"text/css\">
+        <link href=\"css/index.css\" rel=\"stylesheet\" type=\"text/css\">
+	</head>
+	<body>
+		<img src=\"image/logoLungoSMALL.png\" width=\"1120\" height=\"150\" alt=\"\"/>
+		<div id=\"menù\">
+			<div id=\"a\"><img src=\"image/areaUtente.png\" width=\"177,2\" height=\"59,1\" alt=\"\"/></div>
+			<div id=\"b\"><img src=\"image/squadra.png\" width=\"177,2\" height=\"59,1\" alt=\"\"/></div>
+			<div id=\"c\"><img src=\"image/mercato.png\" width=\"177,2\" height=\"59,1\" alt=\"\"></div>
+			<div id=\"d\"><img src=\"image/cerca.png\" width=\"177,2\" height=\"59,1\" alt=\"\"></div>
+			<div id=\"e\"><img src=\"image/logout.png\" width=\"177,2\" height=\"59,1\" alt=\"\"></div>
+		</div>
+	</body>
+</html>";
+?>
