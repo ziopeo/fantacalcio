21448a4507b16d318e7c757e62231440f7239160
Stefano Foresta <forestastefano@gmail.com>
2016-01-24 01:12:38 +0100

    Creazione delle pagine di: - connessione al database "db_con.php" REALIZZATA - da testare - pagina di login "login.php" DA REALIZZARE - pagina per la registrazione sulla piattaforma "registrazione.php" DA REALIZZARE

---
+++ Fantacalcio/WebApplication/db_con.php
@@ -0,0 +1,18 @@
+<?php
+/* Connessione al database*/
+
+$connect = mysql_connect("localhost","root","root");
+
+/* in caso di fallimento della connesione */
+if(!$connect){
+	die('Connessione fallita, errore: '.mysql_error());
+}
+
+/* selezione del database */
+$db_select = mysql_select_db("ufl_db",$connect);
+
+/* in caso di fallimento della selezione del database */
+if(!$db_select){
+	die('Selezione database fallita, errore: '.mysql_error());
+}
+?>
