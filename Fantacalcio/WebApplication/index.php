86c0a403c67c6103c20a2ac0a04adba94a74ea65
Stefano Foresta <forestastefano@gmail.com>
2016-01-24 21:04:14 +0100

    Inserimento "index.php" per l'accesso - PAGINA DA COMPLETARE

---
+++ Fantacalcio/WebApplication/index.php
@@ -0,0 +1,22 @@
+<?php
+	echo"<!doctype html public>
+		<html dir=\"ltr\" lang=\"IT\">
+			<head>
+			<meta charset=\"UTF-8\">
+        	<meta name=\"description\" content=\"UFL - University Fanta League - website application per sfidare i tuoi compagni universitari in una fantalega\">
+        	<meta name=\"keywords\" content=\"università facoltà lega fantalega fantacalcio calcio soccer fantamilioni mercato calciatori\">
+        	<meta name=\"author\" content=\"Foresta Stefano, Franzese Gennaro, Paglialonga Giuseppe\">
+        	<meta name=\"copyright\" content=\"Foresta Stefano, Franzese Gennaro, Paglialonga Giuseppe, UFL\">
+        	<meta name=\"robots\" content=\"index,follow\">
+			<title>UFL - University Fanta League</title>
+		</head>
+
+		<body>
+        	<div class=\"back_imgIndex\">
+				<img id=\"logo\" src=\"image/logoLungoSMALL.png\" alt=\"UFL - University Fanta League\"/>
+				
+        	  	<div class=\"back_imgIndex\"></div>
+        	</div>
+		</body>
+	</html>"
+?>
