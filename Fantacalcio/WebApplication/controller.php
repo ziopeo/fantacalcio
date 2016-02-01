24278d81155fb000046958b61f9e5f0817106773
gennaro franzese <gennnarino@gmail.com>
2016-02-01 17:11:11 +0100

    Query carica leghe

--- Fantacalcio/WebApplication/controller.php
+++ Fantacalcio/WebApplication/controller.php
@@ -1,19 +1,35 @@
-<?php 
+<?php
+include 'db.php';
+function getModelGiocatori() 
+{
 
-include 'model.php';
-include 'vista.php';
+$sql='SELECT * FROM Giocatore';
+return mysqli_query($conn, $sql); 
+}
+function caricaGiocatori($arr)
+{
+$queryInserisci = "INSERT INTO Giocatore(`idgiocatore`, `ruolo`, `nome`, `squadra`, `prezzoIniziale`, `prezzoAttuale`) VALUES ('". $arr[0]."' ,'". $arr[3]."','". $arr[1]."','". $arr[4]."',". $arr[5].",". $arr[6].")";
+return mysqli_query($conn, $queryInserisci);
 
-switch($_POST['metodo']){
-	case 'getGiocatori';
-			$result=getModelGiocatori();
-			stampaGiocatori($result);
-			break;
-	case 'controlla_login';
-			break;
-	default:
-		//homepage default
-		include 'corpo.php';
-		break;
+}
+
+function creaArchivioGiocatori()
+{
+	$idAteneo=getIdAteneoCorrente();
+	$today = date("Y-m-d H:00:00");
+$querycreaArchivio("INSERT INTO `ArchivioLega`(`lega`, `dataUpload`) VALUES (". $idAteneo . ",'". $today ."')";
+$idArchivioLega= $conn->insert_id;
+return $idArchivioLega;
+}
+
+fuction creaUtente($matricola,$nome,$cognome,$facolta,$email,$password,$squadra)
+{
+	$queryInserisci = "Insert into 'utente'('matricola','nome','cognome','email','facoltà','squadra','pass') Values ($matricola,$nome,$cognome,$email,$facolta,$squadra)";
 	
+}
+fuction caricaLega()
+{
+	$sql= "Select nome from leghe";
+	return mysql_fetch_array($sql);
 }
 ?>
--- Fantacalcio/WebApplication/model.php
+++ Fantacalcio/WebApplication/model.php
@@ -1,23 +1,29 @@
-<?php
-include 'db.php';
-function getModelGiocatori() 
-{
+<?php 
 
-$sql='SELECT * FROM Giocatore';
-return mysqli_query($conn, $sql); 
+include 'model.php';
+include 'vista.php';
+
+switch($_POST['metodo']){
+	case 'getGiocatori':
+			$result=getModelGiocatori();
+			stampaGiocatori($result);
+			break;
+	case 'controlla_login':
+			break;
+	case'controlla_registrazione':
+		$matricola = $_POST(matricola_reg);
+		$nome = $_POST(nome_reg);
+		$cognome = $_POST(cognome_reg);
+		$facolta = $_POST(facolta_reg);
+		$email = $_POST(email_reg);
+		$password = $_POST(pass_reg);
+		$squadra= $_POST(squadra_reg);
+		creaUtente($matricola,$nome,$cognome,$facolta,$email,$password,$squadra);
+	default:
+		//homepage default
+		include 'corpo.php';
+		break;
+	case'lista_facolta':
+		listafacolta();
 }
-function caricaGiocatori($arr)
-{
-$queryInserisci = "INSERT INTO Giocatore(`idgiocatore`, `ruolo`, `nome`, `squadra`, `prezzoIniziale`, `prezzoAttuale`) VALUES ('". $arr[0]."' ,'". $arr[3]."','". $arr[1]."','". $arr[4]."',". $arr[5].",". $arr[6].")";
-return mysqli_query($conn, $queryInserisci);
-
-}
-
-function creaArchivioGiocatori()
-{
-	$idAteneo=getIdAteneoCorrente();
-	$today = date("Y-m-d H:00:00");
-$querycreaArchivio("INSERT INTO `ArchivioLega`(`lega`, `dataUpload`) VALUES (". $idAteneo . ",'". $today ."')";
-$idArchivioLega= $conn->insert_id;
-return $idArchivioLega;
 ?>
